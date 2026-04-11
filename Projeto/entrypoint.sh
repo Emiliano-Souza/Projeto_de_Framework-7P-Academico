e2af65e4c1e01c2932bf7320ac5ccf1d9e4a6105
emiliano.fs.junior <emiliano.fs.junior@gmail.com>
2026-04-10 22:03:07 -0300

    Fix Ambiente Docker
    Fix no ambiente docker, resolvido o problema de restart do container do django

--- Projeto/entrypoint.sh
+++ Projeto/entrypoint.sh
@@ -9,38 +9,8 @@ done
 
 echo "Banco pronto."
 
-if [ ! -f manage.py ]; then
-  echo "Criando projeto Django..."
-  django-admin startproject config .
-
-  python <<'PY'
-from pathlib import Path
-import re
-
-p = Path("config/settings.py")
-s = p.read_text()
-
-s = re.sub(r"ALLOWED_HOSTS = \[\]", "ALLOWED_HOSTS = ['*']", s)
-
-s += """
-
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'epi_db'),
-        'USER': os.environ.get('POSTGRES_USER', 'epi_user'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '123456'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
-}
-"""
-p.write_text(s)
-PY
-fi
-
+echo "Rodando migrations..."
 python manage.py migrate --noinput
 
+echo "Subindo servidor..."
 exec python manage.py runserver 0.0.0.0:8000
